a6812065fa759d2af8ed7fe4ccb627ff14b76013
Sharwan Kumar <sharwan981052@gmail.com>
2026-02-24 15:50:44 +0530

    Added multer as middleware

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(cors({
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
-appp.use(cookieParser())
+app.use(cookieParser())
 
 const app = express()
 
---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import {v2 as cloudinary} from cloudinary
+import fs from "fs"
+
+cloudinary.config(
+    {
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY,
+        api_secret: CLOUDINARY_API_SECRET,
+    }
+)
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null;
+        //upload the file on clodinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        console.log(response)
+        //file has been uploaded successfully
+        console.log("File has been uploaded successfully ", response.url)
+    } catch (error) {
+        fs.unlink(localFilePath)
+        //remove the locally saved temporary file as upload operation got failed
+        console.error("Error occured: ", error)
+    }
+}
+
+export {uploadOnCloudinary}
